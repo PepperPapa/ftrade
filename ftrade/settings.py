9032df4e895faf9be89fc17d6c134246250f94f2
pepperpapa <zhongxin2506@outlook.com>
2017-10-19 22:27:35 +0800

    admin页面更新

--- ftrade/settings.py
+++ ftrade/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.locale.LocaleMiddleware', 
+    'django.middleware.locale.LocaleMiddleware',    # 本地化中间件
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
--- goods/admin.py
+++ goods/admin.py
@@ -3,5 +3,8 @@ from django.contrib import admin
 # Register your models here.
 from .models import Category, Goods
 
+class GoodsAdmin(admin.ModelAdmin):
+    list_display = ["name", "recommended", "description", "category", "thumbnail1", "pub_date"]
+
 admin.site.register(Category)
-admin.site.register(Goods)
+admin.site.register(Goods, GoodsAdmin)
